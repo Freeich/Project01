ddbcab568971131bb17c3f271b439626e20bab04
Freeich <1530056567@qq.com>
2020-10-19 23:19:34 +0800

    Initial commit

---
+++ Module1/src/com/test/bean/Customers.java
@@ -0,0 +1,8 @@
+package com.test.bean;
+
+public class Customers {
+    public static void main(String[] args) {
+        System.out.println("bean!!!");
+
+    }
+}
---
+++ Module1/src/com/test/bean/Order.java
@@ -0,0 +1,23 @@
+package com.test.bean;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * @author YangZechen
+ * @date 2020/10/19 - 20:07
+ */
+public class Order {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("asdfasdfa");
+        ArrayList list = new ArrayList();
+        list.add(0,123);
+    }
+
+    public void testMai(){
+        for (int i = 0; i <3 ; i++) {
+            
+        }
+    }
+}
